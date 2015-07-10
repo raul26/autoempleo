67d33a9948cedd118a9aa25b5a6c7138ed16f304
raul26 <k_vm26@hotmail.com>
2015-07-10 14:13:27 -0500

    working on secition 2

--- js/personal-2.js
+++ js/personal-2.js
@@ -2,6 +2,9 @@ $(document).ready(function(){
   $('.contenido').hide();
   $('#index').show();
   $('.st-clr').on('click', function(evt){
+    $('.st-clr').removeClass('slct-opt');
+    $('#linea-grande').css('visibility','hidden');
+    $(this).addClass('slct-opt');
     evt.preventDefault();
     $('.contenido').hide();
     var id = '#'+ $(this).data('opt');
