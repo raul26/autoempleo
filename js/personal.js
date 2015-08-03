7bd4c6ab1491604155f7dd601829b1b2db2c257d
raul26 <k_vm26@hotmail.com>
2015-08-03 14:22:52 -0500

    my changes done

--- js/personal.js
+++ js/personal.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
-	
+  $('.contenido').hide();
+  var opt = document.URL.split('?')[1].split('=')[1];
+  $('#'+opt).show();
+  $('.m-'+opt).addClass('slct-opt');
+  $('.st-clr').on('click', function(evt){
+    $('.st-clr').removeClass('slct-opt');
+    $(this).addClass('slct-opt');
+    evt.preventDefault();
+    $('.contenido').hide();
+    var id = '#'+ $(this).data('opt');
+    $(id).show();
+  })
 });
